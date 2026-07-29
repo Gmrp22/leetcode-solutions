9fc4a7f0492ab25d642ec6baffc76563e3c0b307
gmrp22 <gmrp22@hotmail.com>
2026-07-28 20:34:38 -0600

    feat: adding better solution

--- 3. Longest Substring Without Repeating Characters.ts
+++ 3. Longest Substring Without Repeating Characters.ts
@@ -25,3 +25,38 @@ function lengthOfLongestSubstring(s: string): number {
     return finalLen
 };
 
+
+
+function lengthOfLongestSubstringBetter(s: string): number {
+    if (!s) return 0
+    let substring = new Map()
+    //miMap.set(valor, clave);
+    substring.set(s[0], 0)
+    let finalLen = substring.size
+    let izq = 0
+    let der = 0
+
+    for (let x = 1; x < s.length; x++) {
+        der = x
+        let currentChar = s[x]
+        if (substring.has(currentChar)) {
+            izq = Math.max(izq, substring.get(currentChar) + 1)
+            substring.set(currentChar, x)
+        }
+        else {
+
+            substring.set(currentChar, x)
+        }
+
+        let longitudActual = der - izq + 1;
+
+        if (longitudActual > finalLen) {
+            finalLen = longitudActual;
+        }
+    }
+
+
+    return finalLen
+};
+
+
